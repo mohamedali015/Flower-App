798c4cbd07341491b61afa2ef3cb99fc07795bbe
FatmaIsmail101 <ismailfatma197@gmail.com>
2026-05-04 19:35:05 +0300

    Ref: I deleted App Section and used bottom navigation bar directly

--- lib/config/route_manager/route_generator.dart
+++ lib/config/route_manager/route_generator.dart
@@ -1,10 +1,10 @@
 import 'package:flower_app/config/route_manager/routes.dart';
+import 'package:flower_app/core/shared_widgets/custom_bottom_nav.dart';
 import 'package:flower_app/feautre/cart/presentation/screens/cart_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../core/values/app_strings.dart';
-import '../../feautre/app_section/presentation/screen/app_section_screen.dart';
 import '../../feautre/category/presentation/screens/category_screen.dart';
 import '../../feautre/home/presentation/screens/home_screen.dart';
 import '../../feautre/profile/presentation/screens/profile_screen.dart';
@@ -24,7 +24,7 @@ class RouteGenerator {
       case Routes.profileRoute:
         return CupertinoPageRoute(builder: (_) => ProfileScreen());
       case Routes.bottomNavBarRoute:
-        return CupertinoPageRoute(builder: (_) => AppSectionScreen());
+        return CupertinoPageRoute(builder: (_) => CustomBottomNavBar());
 
       /// Default (Unknown Route)
       default:
