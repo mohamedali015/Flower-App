e4a9171d616943c0de200314dadb92f723516b69
Mohamed Ali <mohamed.ali01537@gmail.com>
2026-05-06 22:23:55 +0300

    Refactor register screen layout and improve code formatting

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
       title: 'Flower APP',
 
       initialRoute: (token != null && rememberMe)
-          ? Routes.homeRoute
+          ? Routes.bottomNavBarRoute
           : Routes.loginRoute,
       onGenerateRoute: RouteGenerator.getRoute,
       locale: const Locale("en"),
