57cffaa9e4182054d94d833a546cce7e0b42a26c
Mark raouf <markraouf2001@gmail.com>
2026-05-25 12:24:05 +0300

    feat(profile): PR resolve

--- lib/config/user/manager/user_cubit.dart
+++ lib/config/user/manager/user_cubit.dart
@@ -1,17 +1,17 @@
-import 'package:flower_app/config/user/domain/use_cases/logout_use_case.dart';
 import 'package:flower_app/config/user/manager/user_events.dart';
 import 'package:flower_app/config/user/manager/user_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
+
 import '../../error_handling/result.dart';
 import '../domain/use_cases/get_user_data_use_case.dart';
 
 @lazySingleton
 class UserCubit extends Cubit<UserState> {
-  UserCubit(this._getUserDataUseCase ) : super(UserState());
+  UserCubit(this._getUserDataUseCase) : super(UserState());
 
   final GetUserDataUseCase _getUserDataUseCase;
-  // final LogoutUseCase _logoutUseCase;
+
   bool _handledUnauthorized = false;
 
   /// events
@@ -37,13 +37,10 @@ class UserCubit extends Cubit<UserState> {
           _resetUnauthorized();
           break;
         }
-    /*  case LogoutUserEvent():
-        _logout();*/
     }
   }
 
   Future<void> _getUserData() async {
-    print("called");
     emit(state.copyWith(isLoading: true));
     final response = await _getUserDataUseCase.call();
 
@@ -69,7 +66,4 @@ class UserCubit extends Cubit<UserState> {
 
     emit(state.copyWith(isUnauthorized: true, user: null));
   }
-
-  /*void _logout() {
-  }*/
 }
