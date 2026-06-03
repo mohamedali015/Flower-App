d58d8e692a94b12203b9151b0f1911c46b4f7ecd
Mark raouf <markraouf2001@gmail.com>
2026-06-03 20:31:05 +0300

    feat(address): init Saved Address Screen

--- lib/features/user_address/presentation/screens/saved_addresses_screen.dart
+++ lib/features/user_address/presentation/screens/saved_addresses_screen.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/localization/l10n/app_localizations.dart';
-import '../../../../core/utils/app_colors.dart';
-import '../../../../core/utils/app_text_styles.dart';
 
 class SavedAddressesScreen extends StatelessWidget {
   SavedAddressesScreen({super.key});
