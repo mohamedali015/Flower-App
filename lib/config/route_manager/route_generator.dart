bc059ec38b849d50f98d0ce9dfe19c0a3fb00a25
Mohamed Ali <mohamed.ali01537@gmail.com>
2026-07-06 04:52:11 +0300

    feat(payment): integrate Firebase Crashlytics and update localization strings

--- lib/config/route_manager/route_generator.dart
+++ lib/config/route_manager/route_generator.dart
@@ -19,8 +19,8 @@ import 'package:flower_app/features/orders/presentation/manager/orders_cubit.dar
 import 'package:flower_app/features/orders/presentation/manager/orders_events.dart';
 import 'package:flower_app/features/orders/presentation/pages/orders_screen.dart';
 import 'package:flower_app/features/product_details/presentation/pages/product_details_screen.dart';
-import 'package:flower_app/features/terms_and_conditions/presentation/screens/terms_and_conditions_screen.dart';
 import 'package:flower_app/features/search/presentation/manager/search_cubit.dart';
+import 'package:flower_app/features/terms_and_conditions/presentation/screens/terms_and_conditions_screen.dart';
 import 'package:flower_app/features/user_address/presentation/manger/user_address_cubit.dart';
 import 'package:flower_app/features/user_address/presentation/screens/add_address_screen.dart';
 import 'package:flower_app/features/user_address/presentation/screens/saved_addresses_screen.dart';
@@ -43,13 +43,12 @@ import '../../features/forget_password/presentation/manager/event/forget_passwor
 import '../../features/forget_password/presentation/pages/forget_password_enter_email_view.dart';
 import '../../features/forget_password/presentation/pages/reset_password.dart';
 import '../../features/forget_password/presentation/pages/verify_code.dart';
-import '../../features/payment/views/pages/payment_screen.dart';
-import '../../features/search/presentation/manager/search_cubit.dart';
-import '../add_to_cart/presentation/manager/add_cart_cubit.dart';
 import '../../features/notifications/presentation/pages/notifications_screen.dart';
+import '../../features/payment/views/pages/payment_screen.dart';
 import '../../features/search/presentation/screens/search_screen.dart';
 import '../../features/splash/splash_screen.dart';
 import '../../features/user_address/domain/entities/address.dart';
+import '../add_to_cart/presentation/manager/add_cart_cubit.dart';
 
 abstract class RouteGenerator {
   static Route<dynamic> getRoute(RouteSettings settings) {
@@ -271,8 +270,7 @@ abstract class RouteGenerator {
     return MaterialPageRoute(
       builder: (_) => const Scaffold(
         body: Center(
-          child: Text(AppStrings.pageNotFound,
-              style: TextStyle(fontSize: 18)),
+          child: Text(AppStrings.pageNotFound, style: TextStyle(fontSize: 18)),
         ),
       ),
     );
